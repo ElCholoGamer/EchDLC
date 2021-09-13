1813d22665c0095e0d1831e5714e7843f71b21a2
ElCholoGamer <josedanielgrayson@gmail.com>
2021-09-12 21:08:48 -0500

    add ech pacifier

---
+++ Items/EchPacifier.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EchDLC.Items
+{
+    public class EchPacifier : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Catnip");
+            Tooltip.SetDefault("'Cat Crack'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 34;
+            item.useAnimation = item.useTime = 45;
+            item.noMelee = true;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.UseSound = SoundID.Item57;
+            item.consumable = true;
+            item.maxStack = 99;
+            item.value = 1;
+            item.rare = ItemRarityID.Purple;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            player.GetModPlayer<EchPetPlayer>().echTime = 0;
+            Main.NewText("Echdeath has been pleased.", Color.LightGreen, false);
+
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            Mod soulsMod = ModLoader.GetMod("FargowiltasSouls");
+            Mod fargoMod = ModLoader.GetMod("Fargowiltas");
+
+            recipe.AddIngredient(soulsMod, "EternitySoul");
+            recipe.AddIngredient(soulsMod, "Sadism", 15);
+            recipe.AddIngredient(soulsMod, "MutantScale", 15);
+            recipe.AddIngredient(soulsMod, "DeviatingEnergy", 15);
+            recipe.AddIngredient(ItemID.Bottle);
+            recipe.AddTile(fargoMod, "CrucibleCosmosSheet");
+            recipe.SetResult(this);
+
+            recipe.AddRecipe();
+        }
+    }
+}
